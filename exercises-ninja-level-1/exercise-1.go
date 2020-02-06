c549ab4dc491dc72e3d41b18181dbbffd41569ff
living-tribunal <mno239@gmail.com>
2020-02-06 14:55:39 +0530

    updates based on solution

--- exercises-ninja-level-1/exercise-1.go
+++ exercises-ninja-level-1/exercise-1.go
@@ -3,12 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var x int = 42
-	var y string = "James Bond"
-	var z bool = true
+	x := 42
+	y := "James Bond"
+	z := true
 	fmt.Println(x, y, z)
 	fmt.Println("Interger is:", x)
-	fmt.Println("String is:", y)
-	fmt.Println("Boolean is:", z)
-
+	fmt.Println("String is: ", y)
+	fmt.Println("Boolean is: ", z)
 }
