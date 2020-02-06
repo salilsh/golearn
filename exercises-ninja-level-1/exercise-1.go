d13a2178fcb43dba19cdd58850253d6d413912a0
living-tribunal <mno239@gmail.com>
2020-02-06 14:36:32 +0530

    first exercise. yippee

---
+++ exercises-ninja-level-1/exercise-1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 42
+	var y string = "James Bond"
+	var z bool = true
+	fmt.Println(x, y, z)
+	fmt.Println("Interger is:", x)
+	fmt.Println("String is: ", y)
+	fmt.Println("Boolean is: ", z)
+
+}
