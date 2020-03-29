3155ed351f5dc4238f36aef8d67bad845bbf05ac
Salil Shankar <mno239@gmail.com>
2020-03-29 12:02:39 +0530

    updated code for ninja level 10 exercise 7

--- exercises-ninja-level-10/exercise-7/exercise-7.go
+++ exercises-ninja-level-10/exercise-7/exercise-7.go
@@ -2,34 +2,43 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 
 	ch := make(chan int)
 
-	for k := 1; k < 11; k++ {
-		go writeToChannel(ch, ((k * 10) + 1))
+	//implementing by looking at fellow student's work. lesson: if a channel is closed before values are even received from it, it leads to a deadlock.
+	go func() {
+		for k := 1; k < 11; k++ {
+			wg.Add(1)
+			go writeToChannel(ch, ((k * 10) + 1))
 
-	}
+		}
+		wg.Wait()
+		close(ch)
+	}()
 
 	readValues(ch)
-
-	close(ch)
 
 	fmt.Println("Exiting....")
 }
 
 func writeToChannel(c chan<- int, end int) {
 	for i := end - 10; i < end; i++ {
-		fmt.Println("writing value", i, "to channel")
+		//fmt.Println("writing value", i, "to channel")
 		c <- i
+
 		//fmt.Println("ROUTINES", runtime.NumGoroutine())
 	}
+	wg.Done()
 
 }
 
-func readValues(c <-chan int) {
+/* func readValues(c <-chan int) {
 
 	for i := 1; i < 101; i++ {
 
@@ -37,9 +46,9 @@ func readValues(c <-chan int) {
 		fmt.Println(i, <-c)
 	}
 
-}
+} */
 
-/* func readValues(c <-chan int) {
+func readValues(c <-chan int) {
 	i := 1
 	for {
 		select {
@@ -54,4 +63,4 @@ func readValues(c <-chan int) {
 			}
 		}
 	}
-}*/
+}
