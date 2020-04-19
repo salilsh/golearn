e60f7835480eb629a6064498ff2c39a27f58ca32
Salil Shankar <Salil@Salils-MacBook-Pro.local>
2020-04-19 20:41:05 +0530

    adding exercise 12

---
+++ exercises-ninja-level-12/exercise-1/dog/dog.go
@@ -0,0 +1,7 @@
+//Package dog takes human years and returns corresponding dog years. 
+package dog
+
+//Dogyears function takes human years and returns dog years. 
+func Dogyears(humanyears int) int {
+	return (humanyears * 7)
+}
---
+++ exercises-ninja-level-12/exercise-1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"dog"
+	"fmt"
+)
+
+func main() {
+	dogyears := dog.Dogyears(7)
+	fmt.Println(dogyears)
+}
